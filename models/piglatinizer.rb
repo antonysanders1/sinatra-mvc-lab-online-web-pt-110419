991f488fbf3445dcbec7f848a91857a46050049d
Antony Sanders <antony.devon92@gmail.com>
2020-02-20 23:47:11 +0000

    Automatically backed up by Learn

---
+++ models/piglatinizer.rb
@@ -0,0 +1,18 @@
+class PigLatinizer
+  attr_reader :text
+  
+  def initialize(text)
+    @text = text.downcase
+  end 
+  
+  def collect_words 
+    
+    words_arr = text.split(" ")
+    words_arr
+    
+    arr.
+  end 
+  
+  
+  
+end 
